20b9439efdd13e1914a53615bf7ec360aa54a594
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-05-25 14:55:49 +0300

    implement redux toolkit

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,33 @@
-import React from "react";
+// ./src/features/todos/CreateTodo.js
+import React, { useState } from "react";
+import { todoAdded } from "./todosSlice";
+import { useDispatch } from "react-redux";
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
+  function handleChange(event) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch(todoAdded(text))
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>add todo</label>
+          <input type="text" onChange={handleChange} value={text} />
+        </p>
+        <input type="submit" />
+      </form>
+      <p>Form Text: {text}</p>
+    </div>
+  );
 }
 
 export default CreateTodo;
